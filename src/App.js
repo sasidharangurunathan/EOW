05ad67a05591d76022c3dd2e5213c2c4516ca6f1
sasidharan <sasidharang32@gmail.com>
2024-08-17 06:13:16 -0700

    image

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
+import './App.css';
+import imags from './image/sasi.JPG';
 
 function App() {
   return (
     <div>
+      <div class="header">
+        <a href="#default" class="logo">EOW</a>
+        <div class="header-right">
+          <a class="active" href="#home">Home</a>
+          <a href="#contact">Contact</a>
+          <a href="#about">About</a>
+        </div>
+      </div>
       <>
-    <center><p>EOW On Watchs</p>
-    <h1>Our Team mates</h1></center>
-    <div>
-    <li>Anand</li>
-    <li>Ganesh</li>
-    <li>Logesh</li>
-    <li>Sasidharan</li>
-    <li>Deepan</li>
-    </div>
+        <center><p>EOW On Watchs</p>
+          <h1>Our Team mates</h1></center>
+
+        <><div class="card">
+          <img src={imags} alt="Logo" />
+          <h1>Sasidharan G</h1>
+          <p>Associate software Developer</p>
+        </div>
+        </>
+
+        <div>
+          <li>Anand</li>
+          <li>Ganesh</li>
+          <li>Logesh</li>
+          <li>Sasidharan</li>
+          <li>Deepan</li>
+        </div>
       </>
     </div>
   );
 }
 
 export default App;
+
